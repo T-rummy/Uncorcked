b850aed9bfd8f15ef60f1d8608f70dd6b6684cc0
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-08-31 20:23:23 -0500

    something wring with push

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,12 @@
 
+var modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 // Get the modal
 var modal = document.getElementById('id02');
