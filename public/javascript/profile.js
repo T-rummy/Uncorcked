3639312b51a8ef681f81142d672700549aeba4ad
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-09-04 19:09:11 -0500

    redo login/homepage

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -5,3 +5,39 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.height = "100px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.height = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav1() {
+  document.getElementById("mySidenav2").style.height = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav1() {
+  document.getElementById("mySidenav2").style.height = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav2() {
+  document.getElementById("mySidenav3").style.height = "350px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav2() {
+  document.getElementById("mySidenav3").style.height = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
